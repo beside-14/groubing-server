f61576c1f81dd421c9f09e84b7d00a8e75ec6917
SeongHeon Sim <deep@osean.me>
2023-04-05 00:39:01 +0900

    fix : Import 와일드 카드 제거

--- src/main/kotlin/com/beside/groubing/groubingserver/global/config/AuditingConfig.kt
+++ src/main/kotlin/com/beside/groubing/groubingserver/global/config/AuditingConfig.kt
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.data.domain.AuditorAware
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.security.core.context.SecurityContextHolder
-import java.util.*
+import java.util.Optional
 
 @Configuration
 @EnableJpaAuditing
