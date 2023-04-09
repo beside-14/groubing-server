608596c2178780c38c7e0232862d5a00769d330c
SeongHeon Sim <deep@osean.me>
2023-04-09 16:30:02 +0900

    feat : CreateBingoApi > @RequestMapping 삭제

--- src/main/kotlin/com/beside/groubing/groubingserver/domain/bingo/api/CreateBingoApi.kt
+++ src/main/kotlin/com/beside/groubing/groubingserver/domain/bingo/api/CreateBingoApi.kt
@@ -11,16 +11,14 @@ import org.springframework.validation.BindingResult
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/bingos")
 class CreateBingoApi(
     private val createBingoService: CreateBingoService
 ) {
 
-    @PostMapping
+    @PostMapping("/api/bingos")
     fun create(
         @AuthenticationPrincipal
         member: Member,
