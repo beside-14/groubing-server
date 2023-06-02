f4a0ac8db0a9ae6c717a674e70871e2acf42a30b
SeongHeon Sim <deep@osean.me>
2023-06-02 17:12:20 +0900

    feat(FileDownloadApi) : REST API 규칙 반영

--- src/main/kotlin/com/beside/groubing/groubingserver/global/domain/file/api/FileDownloadApi.kt
+++ src/main/kotlin/com/beside/groubing/groubingserver/global/domain/file/api/FileDownloadApi.kt
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/download")
+@RequestMapping("/api/files")
 class FileDownloadApi(
     private val fileInfoService: FileInfoService
 ) {
