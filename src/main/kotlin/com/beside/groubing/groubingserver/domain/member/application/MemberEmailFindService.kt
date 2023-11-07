b7ffd3b07e29f32ce1917e6dd737e20c730ac752
SeongHeon Sim <deep@osean.me>
2023-11-08 00:32:05 +0900

    fix(Member): 프로필 수정 로직 @Transactional 적용

--- src/main/kotlin/com/beside/groubing/groubingserver/domain/member/application/MemberEmailFindService.kt
+++ src/main/kotlin/com/beside/groubing/groubingserver/domain/member/application/MemberEmailFindService.kt
@@ -3,8 +3,10 @@ package com.beside.groubing.groubingserver.domain.member.application
 import com.beside.groubing.groubingserver.domain.member.dao.MemberFindDao
 import com.beside.groubing.groubingserver.domain.member.payload.response.MemberEmailFindResponse
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
+@Transactional(readOnly = true)
 class MemberEmailFindService(
     private val memberFindDao: MemberFindDao
 ) {
--- src/main/kotlin/com/beside/groubing/groubingserver/global/domain/file/application/FileInfoService.kt
+++ src/main/kotlin/com/beside/groubing/groubingserver/global/domain/file/application/FileInfoService.kt
@@ -4,8 +4,10 @@ import com.beside.groubing.groubingserver.global.domain.file.domain.FileInfoRepo
 import com.beside.groubing.groubingserver.global.domain.file.exception.FileInfoInputException
 import org.springframework.core.io.Resource
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
+@Transactional
 class FileInfoService(
     private val fileInfoRepository: FileInfoRepository
 ) {
