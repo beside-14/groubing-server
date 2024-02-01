e1aa047c02d1df08891bb3feff815475a6aada8b
SeongHeon Sim <deep@osean.me>
2024-02-02 01:05:12 +0900

    feature(Config): 로그 관련 설정 추가

--- src/main/kotlin/com/beside/groubing/groubingserver/global/config/WebConfig.kt
+++ src/main/kotlin/com/beside/groubing/groubingserver/global/config/WebConfig.kt
@@ -1,15 +1,24 @@
 package com.beside.groubing.groubingserver.global.config
 
+import com.beside.groubing.groubingserver.global.interceptor.LoggingInterceptor
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class WebConfig : WebMvcConfigurer {
+class WebConfig(
+    private val loggingInterceptor: LoggingInterceptor
+) : WebMvcConfigurer {
     override fun addArgumentResolvers(argumentResolvers: MutableList<HandlerMethodArgumentResolver>) {
         val resolver = PageableHandlerMethodArgumentResolver()
         resolver.setOneIndexedParameters(true)
         argumentResolvers.add(resolver)
+    }
+
+    override fun addInterceptors(registry: InterceptorRegistry) {
+        registry.addInterceptor(loggingInterceptor)
+            .addPathPatterns("/api/**")
     }
 }
---
+++ src/main/kotlin/com/beside/groubing/groubingserver/global/interceptor/LoggingInterceptor.kt
@@ -0,0 +1,49 @@
+package com.beside.groubing.groubingserver.global.interceptor
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import mu.KLogging
+import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Component
+import org.springframework.web.servlet.HandlerInterceptor
+import org.springframework.web.util.ContentCachingResponseWrapper
+
+@Component
+class LoggingInterceptor(
+    private val objectMapper: ObjectMapper
+) : HandlerInterceptor {
+
+    companion object : KLogging()
+
+    override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
+        logger.info {
+            "Request  >>> ${request.method} ${request.requestURI} ${request.contentType} ${
+                objectMapper.writeValueAsString(
+                    request.parameterMap
+                )
+            }"
+        }
+        return true;
+    }
+
+    override fun afterCompletion(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        handler: Any,
+        ex: Exception?
+    ) {
+        val responseWrapper = response as ContentCachingResponseWrapper
+        logger.info {
+            "Response >>> ${request.method} ${request.requestURI} ${responseWrapper.contentType} ${
+                HttpStatus.valueOf(
+                    responseWrapper.status
+                )
+            } ${
+                String(
+                    responseWrapper.contentAsByteArray
+                )
+            }"
+        }
+    }
+}
